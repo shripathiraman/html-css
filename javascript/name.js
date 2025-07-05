254b863c7ce2d454c0d0d2794c7685cd7992050b
Shri <shripathiram@gmail.com>
2025-07-05 20:03:07 +0530

    Java Script functions added

---
+++ javascript/name.js
@@ -0,0 +1,5 @@
+function name1() {
+    var input1 = document.getElementById('inputField').value;
+    var input2 = document.getElementById('inputField2').value;
+    document.getElementById('output').innerHTML = 'Welcome ' + input1 + ' (' + input2 + ')';
+}
